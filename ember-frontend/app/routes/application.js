b16b701086e48b743d96b93d8edc6f5294070850
tmtocb <kittz.esport@gmail.com>
2021-02-23 03:28:18 +0100

    add Ember.js basic app template, hardcode routing model

--- ember-frontend/app/routes/application.js
+++ ember-frontend/app/routes/application.js
@@ -2,6 +2,21 @@ import Route from '@ember/routing/route';
 
 export default class ApplicationRoute extends Route {
   model(){
-    return 'Testing routing'
+    return [{
+        nick: 'dupreeh',
+        earnings: 1890000
+      }, {
+        nick: 'TaZ',
+        earnings: 590000
+      }, {
+        nick: 'flamie',
+        earnings: 845000
+      }, {
+        nick: 'JW',
+        earnings: 907000
+      }, {
+        nick: 'Skadoodle',
+        earnings: 437479
+      }]
   }
 };
