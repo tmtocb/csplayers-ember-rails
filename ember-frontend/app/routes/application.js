4706849ec7cd1500395ae10b1bc5a551e55c2f79
tmtocb <kittz.esport@gmail.com>
2021-02-23 13:44:54 +0100

    add Player model to Ember.js, remove hardcoded routing data

--- ember-frontend/app/routes/application.js
+++ ember-frontend/app/routes/application.js
@@ -2,21 +2,6 @@ import Route from '@ember/routing/route';
 
 export default class ApplicationRoute extends Route {
   model(){
-    return [{
-        nick: 'dupreeh',
-        earnings: 1890000
-      }, {
-        nick: 'TaZ',
-        earnings: 590000
-      }, {
-        nick: 'flamie',
-        earnings: 845000
-      }, {
-        nick: 'JW',
-        earnings: 907000
-      }, {
-        nick: 'Skadoodle',
-        earnings: 437479
-      }]
+    return this.store.findAll('player');
   }
 };
